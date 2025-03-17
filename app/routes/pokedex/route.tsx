1d4c65ed412ac3f320ce60685316b0a85b8073a1
Yael Pilar <yaelpilarluque@gmail.com>
2025-03-17 14:09:44 +0000

    first try

--- app/routes/pokedex/route.tsx
+++ app/routes/pokedex/route.tsx
@@ -1,11 +1,10 @@
 import Yae from "./components/Yae";
-import Manu from "./components/Manu";
 
 export default function () {
   return (
     <>
       <Yae />
-      <Manu />
+      
     </>
   );
 }
